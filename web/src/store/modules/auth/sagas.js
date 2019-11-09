72bd079ca74c536d0bb5195428e255e524feb101
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-11-09 14:31:23 -0300

    fix: @web/Recover lost progress

--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -1,3 +1,35 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+import history from '~/services/history';
+
+import { signInSuccess, signFailure } from './actions';
+
+function* singIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, '/sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    if (!user.provider) {
+      toast.error('Somente providers tem acesso a essa aplicação');
+      yield put(signFailure());
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('Dashboard');
+  } catch (error) {
+    toast.error('Credenciais inválidas');
+    yield put(signFailure());
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', singIn)]);
