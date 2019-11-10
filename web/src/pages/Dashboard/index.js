4b5ec0067666ecc28384adaecf97172dc18d1a6a
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-11-10 02:19:44 -0300

    feat: @web/Dashboard styles

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, Time } from './styles';
+
+import api from '~/services/api';
 
 export default function Dashboard() {
-  return <Container>Dashboard</Container>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft color="#fff" size={36} />
+        </button>
+        <strong>31 de maio</strong>
+        <button type="button">
+          <MdChevronRight color="#fff" size={36} />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Raphael de Oliveira</span>
+        </Time>
+        <Time>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time available>
+          <strong>10:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Em aberto</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
