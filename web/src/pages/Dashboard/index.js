14762a8de964aa6864615dabfef68c3b6808ee45
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-11-16 13:51:30 -0300

    feat: @web/schedules with timezone

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,21 +1,60 @@
-import React, { useState, useMemo } from 'react';
-import format from 'date-fns/format';
-import subDays from 'date-fns/subDays';
-import addDays from 'date-fns/addDays';
+import React, { useState, useMemo, useEffect } from 'react';
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import { utcToZonedTime } from 'date-fns-tz';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 import { Container, Time } from './styles';
 
 import api from '~/services/api';
 
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
 export default function Dashboard() {
   const [date, setDate] = useState(new Date());
+  const [schedules, setSchedules] = useState([]);
 
   const dateFormatted = useMemo(
     () => format(date, "d 'de' MMMM", { locale: pt }),
     [date]
   );
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('/schedules', {
+        params: { date },
+      });
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const comparedDate = utcToZonedTime(checkDate, timezone);
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(comparedDate, new Date()),
+          appointment: response.data.find(schedule =>
+            isEqual(parseISO(schedule.date), comparedDate)
+          ),
+        };
+      });
+
+      setSchedules(data);
+    }
+
+    loadSchedule();
+  }, [date]);
 
   function handlePrevDay() {
     setDate(subDays(date, 1));
@@ -38,22 +77,20 @@ export default function Dashboard() {
       </header>
 
       <ul>
-        <Time past>
-          <strong>08:00</strong>
-          <span>Raphael de Oliveira</span>
-        </Time>
-        <Time>
-          <strong>09:00</strong>
-          <span>Em aberto</span>
-        </Time>
-        <Time available>
-          <strong>10:00</strong>
-          <span>Em aberto</span>
-        </Time>
-        <Time>
-          <strong>11:00</strong>
-          <span>Em aberto</span>
-        </Time>
+        {schedules.map(schedule => (
+          <Time
+            key={schedule.time}
+            past={schedule.past}
+            available={!schedule.appointment}
+          >
+            <strong>{schedule.time}</strong>
+            <span>
+              {schedule.appointment
+                ? schedule.appointment.user.name
+                : 'Em aberto'}
+            </span>
+          </Time>
+        ))}
       </ul>
     </Container>
   );
