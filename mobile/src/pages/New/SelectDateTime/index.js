1a7f8698dd5c115685b2e5332d749566a3852433
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-11-21 22:46:20 -0300

    feat: @mobile/Select hour list page

--- mobile/src/pages/New/SelectDateTime/index.js
+++ mobile/src/pages/New/SelectDateTime/index.js
@@ -1,19 +1,58 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import PropTypes from 'prop-types';
 
-import {Container} from './styles';
+import {Container, HourList, Hour, Title} from './styles';
 
 import Background from '~/components/Background';
 import DateInput from '~/components/DateInput';
 
-export default function SelectDateTime() {
+import api from '~/services/api';
+
+export default function SelectDateTime({navigation}) {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    async function loadAvailable() {
+      const response = await api.get(`/providers/${provider.id}/availables`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+
+      setHours(response.data);
+    }
+
+    loadAvailable();
+  }, [date, provider.id]);
+
+  function handleSelectHour(time) {
+    navigation.navigate('Confirm', {
+      provider,
+      time,
+    });
+  }
 
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+
+        <HourList
+          data={hours}
+          keyExtractor={item => String(item.time)}
+          renderItem={({item}) => (
+            <Hour
+              onPress={() => handleSelectHour(item.value)}
+              enabled={item.available}>
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
@@ -30,3 +69,10 @@ SelectDateTime.navigationOptions = ({navigation}) => ({
     </TouchableOpacity>
   ),
 });
+
+SelectDateTime.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+    navigate: PropTypes.func,
+  }).isRequired,
+};
